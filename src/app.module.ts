9e66bded6744203961d95a997d127614e4f9365d
afgomezv <afgomezvonline@gmail.com>
2024-11-12 18:35:07 -0500

    graphql  todos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { HelloWorldModule } from './hello-world/hello-world.module';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
@@ -15,6 +16,8 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
     }),
 
     HelloWorldModule,
+
+    TodoModule,
   ],
   controllers: [],
   providers: [],
